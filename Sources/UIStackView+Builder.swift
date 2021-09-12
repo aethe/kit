54232c53e2f47f7af1eb866adde6efa9be32aa63
Andrey Ufimcev <aethe.dev@gmail.com>
2021-09-12 16:17:21 +0100

    Add UIKit import to UIStackView+Builder

--- Sources/UIStackView+Builder.swift
+++ Sources/UIStackView+Builder.swift
@@ -20,6 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+import UIKit
+
 extension UIStackView {
     static var horizontal: Builder {
         Builder(axis: .horizontal)
