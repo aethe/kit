2745b53abd5b5ceef0aa325ae2a96913a290519d
Andrey Ufimcev <aethe.dev@gmail.com>
2021-09-12 16:18:05 +0100

    Add UIColor+Code

---
+++ Sources/UIColor+Code.swift
@@ -0,0 +1,32 @@
+// MIT License
+//
+// Copyright (c) 2021 Andrey Ufimcev
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import UIKit
+
+extension UIColor {
+    convenience init(code: UInt32, alpha: CGFloat = 1) {
+        let red = CGFloat((code & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((code & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(code & 0x0000FF) / 255.0
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
